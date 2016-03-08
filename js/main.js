bd615b4982afaf59cdd27cad634b0c6146d8e7c7
Amos Marsters <amosmarsters@Amoss-MacBook-Pro.local>
2016-03-08 14:56:24 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -40,13 +40,13 @@ var writeBerries = function (berries) {
 
   bugs.forEach(function (berry) {
     document.write('<li>');
+
     document.write('<h2>' + berry.name + '</h2>');
+
     document.write('<dl>');
-    document.write('<dt>Family</dt>');
-    document.write('<dd><i>' + berry.family + '</i></dd>');
-    document.write('<dt>Size</dt>');
-    document.write('<dd>' + berry.size[0] + '–' + berry.size[1] + ' mm</dd>');
-    document.write('<dt>Eats</dt>');
+
+    document.write('<dt>Latin</dt>');
+    document.write('<dd><i>' + berry.latin + '</i></dd>');
 
     document.write('<dd>');
 
@@ -54,21 +54,27 @@ var writeBerries = function (berries) {
       document.write('Yes');
     else {
       document.write('no');
-    }
-  }
-
-
+      },
+    },
 
     document.write('</dd>');
 
+    document.write('<dt>Energy</dt>');
+    document.write('<dd>' + berry.energy[0] + ' kJ</dd>');
+    document.write('<dt>Carbs</dt>');
+    document.write('<dd>' + berry.carbs[0] + ' g</dd>');
+    document.write('<dt>Protein</dt>');
+    document.write('<dd>' + berry.protein[0] + ' g</dd>');
+
     document.write('</dl>');
+
     document.write('</li>');
   });
 
   document.write('</ol>');
 };
 
-writeInsects(insects);
+writeBerries(berries);
 
 
 // ##########################################################
